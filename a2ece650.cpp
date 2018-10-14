decabdee8faee26d611844a2ad3c6b38089a7403
Chunyu Mao <c5mao@ecelinux3.uwaterloo.ca>
2018-10-14 12:09:47 -0400

    Code Updated

--- a2ece650.cpp
+++ a2ece650.cpp
@@ -4,9 +4,21 @@
 
 using namespace std;
 
+class line{
+  public:
+    unsigned int srt; //srt vertex of line
+    unsigned int dst; //dst vertex of line
+};
+
+class line_db{
+  public:
+    unsigned int no_of_vertices;
+    line edge[];
+};
+
 vector<string> parse_line(string line){
     vector<string> parsed_line;
-
+    line_db db;
     cout << line[0] << endl;
     if (line[0]=='V'){
     	// Find position of ':' using find() 
@@ -15,6 +27,10 @@ vector<string> parse_line(string line){
         string value = line.substr(pos + 1); 
         cout << value << endl;
         parsed_line.push_back(value);
+        db.no_of vertices = 2;
+        db.edge[0].srt = 0;
+        db.edge[0].dst = 1;
+        cout << "The edge is from %d to %d", db.edge[0].src, db.edge[0].dst <<endl
     }
 
 
@@ -25,38 +41,35 @@ vector<string> parse_line(string line){
 
 int main(){
 
-	string line;
-	vector<string> command;
-	uint i;
+    string line;
+    vector<string> command;
+    unsigned int i;
 
-	while(1){
+    while(1){
+        try{
+            //Read input  
+            getline(cin, line);
+            if(!cin){
+               if(cin.eof())
+               cout << "Error: EOF\n";
+            else
+                cout << "Error: other failure\n";
+            break;
+            }
+            command = parse_line(line);
+            cout << "parse line done" << endl;
+            unsigned int n = command.size();
+            cout << "length of the command:" << n << endl;
 
-	    getline(cin, line);
-	    cout << line << endl;
-        if(!cin){
-           if(cin.eof())
-             cout << "EOF\n";
-           else
-             cout << "other failure\n";
-           
-           break;
-
-        }
-        command = parse_line(line);
-        cout << "parse line done" << endl;
-        uint n = command.size();
-        cout << "length of the command:" << n << endl;
-
-        for(i=0;i<n;i++){
+            for(i=0;i<n;i++){
         	cout << command[i] << endl;
-        }
+            }
+       } catch (const char* msg){
+            cerr << msg <<endl;
+       }
+    }
 
-
-
-	}
-
-	cout << "Error: empty input, exit";
-
-	return 0;
+    cout << "Error: empty input, exit" << endl;
+    return 0;
 
 }
